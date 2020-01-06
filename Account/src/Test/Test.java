737d97f3c38178c59f2e6e321ed9134ae7ecaa34
KaiJiun <kaijiun820@gmail.com>
2020-01-06 19:22:59 +0800

    uses the testing code to debug

--- Account/src/Test/Test.java
+++ Account/src/Test/Test.java
@@ -1,17 +1,21 @@
 package Test;
 
+import Account.Account;
+import Account.Account.Type;
+import Account.Ledger;
+
 import java.util.Date;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class Test {
-	
-	Ledger ledger;
+
+    Ledger ledger;
     Account cash;
     Account anne;
-	
-	@Before
+
+    @Before
     public void setUp() {
         ledger = new Ledger();
         cash = ledger.newAccount("Cash", Type.ASSET);
@@ -19,8 +23,8 @@ public class Test {
         ledger.newAccount("Dues Received", Type.INCOME);
         ledger.newAccount("Tickets Purchased", Type.EXPENSE);
     }
-	
-	@Test
+
+    @Test
     public void testNestedAccounts() {
         Account a;
 
@@ -30,8 +34,8 @@ public class Test {
         a = ledger.getAccount("Cash:Anne");
         assertEquals(anne, a);
     }
-	
-	@Test
+
+    @Test
     public void makePosting() throws AccountingException {
         ledger.newPosting(new Date(), "Received $10 from Anne")
                 .debit("Cash:Anne", 1000)
@@ -48,4 +52,68 @@ public class Test {
         assertEquals(1000, duesBalance);
     }
 
+    @Test(expected = NonExistantAccountException.class)
+    public void makePostWithBadAccount() throws NonExistantAccountException, PostingUnbalancedException {
+        ledger.newPosting(new Date(), "Received $10 from Bob")
+                .debit("Cash:Bob", 1000)
+                .credit("Dues Received", 1000)
+                .post();
+    }
+
+    @Test(expected = PostingUnbalancedException.class)
+    public void makeUnbalancedPost() throws NonExistantAccountException, PostingUnbalancedException {
+        ledger.newPosting(new Date(), "Received $10 from Anne")
+                .debit("Cash:Anne", 900)
+                .credit("Dues Received", 1000)
+                .post();
+    }
+
+    @Test
+    public void forgetToPost() throws AccountingException {
+        ledger.newPosting(new Date(), "Received $10 from Anne")
+                .debit("Cash:Anne", 1000)
+                .credit("Dues Received", 1000);
+
+        int cashBalance = ledger.getAccount("Cash").getTrialBalance();
+        assertEquals(0, cashBalance);
+
+        int anneBalance = ledger.getAccount("Cash:Anne").getTrialBalance();
+        assertEquals(0, anneBalance);
+
+        int duesBalance = ledger.getAccount("Dues Received").getTrialBalance();
+        assertEquals(0, duesBalance);
+    }
+
+    @Test
+    public void makeSureExceptionDoesntPost() {
+        try {
+            makeUnbalancedPost();
+        } catch (AccountingException e) {
+            // Exactly
+        }
+
+        int cashBalance = ledger.getAccount("Cash").getTrialBalance();
+        assertEquals(0, cashBalance);
+
+        int anneBalance = ledger.getAccount("Cash:Anne").getTrialBalance();
+        assertEquals(0, anneBalance);
+
+        int duesBalance = ledger.getAccount("Dues Received").getTrialBalance();
+        assertEquals(0, duesBalance);
+
+        try {
+            makePostWithBadAccount();
+        } catch (AccountingException ex) {
+            // Exactly
+        }
+
+        cashBalance = ledger.getAccount("Cash").getTrialBalance();
+        assertEquals(0, cashBalance);
+
+        anneBalance = ledger.getAccount("Cash:Anne").getTrialBalance();
+        assertEquals(0, anneBalance);
+
+        duesBalance = ledger.getAccount("Dues Received").getTrialBalance();
+        assertEquals(0, duesBalance);
+    }
 }
